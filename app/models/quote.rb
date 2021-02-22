8eafc38b83e40f67ff93fead2a51234be33711ab
rabutarb <rabutarb@andrew.cmu.edu>
2021-02-22 03:48:01 +0300

    Designed home page

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,10 @@
 class Quote < ApplicationRecord
+# Method to get a random quote for home page #
+# @return [Quote]
+# the random quote
+validates_presence_of :body
+validates_presence_of :source
+    def self.get_random_quote
+        self.all.sample
+    end
 end
